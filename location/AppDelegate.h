2644164fa99a9956d21cec4780a732390a144494
yutaozhao <911791332@qq.com>
2018-09-25 11:38:39 +0800

    Initial Commit

---
+++ location/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  location
+//
+//  Created by yutaozhao on 25/9/18.
+//  Copyright © 2018年 yutaozhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ location/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  location
+//
+//  Created by yutaozhao on 25/9/18.
+//  Copyright © 2018年 yutaozhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
